35507bafc66dfaf65fca8f64dcc9fd902852b2d9
Grzegorz Rajchman <grzegorz.rajchman@talis.com>
2023-10-09 09:22:40 +0100

    feat: add addLabels utility

--- lib/common/index.ts
+++ lib/common/index.ts
@@ -1,5 +1,6 @@
 export * from "./container-props";
 export * from "./env-util";
+export * from "./metadata-util";
 export * from "./name-util";
 export * from "./statefulset-util";
 export * from "./value-util";
