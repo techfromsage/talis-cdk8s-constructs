05a2b25efa2dc125a49ea6f588f6b0545e0f5ac4
Grzegorz Rajchman <grzegorz.rajchman@technologyfromsage.com>
2024-02-15 17:15:23 +0000

    test: allow to check locally next release

--- release.config.js
+++ release.config.js
@@ -1,21 +1,51 @@
-module.exports = {
-  branches: ["main"],
-  preset: "conventionalcommits",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/npm",
-      {
-        npmPublish: false,
-        tarballDir: ".",
-      },
+const { execSync } = require("child_process");
+
+function isDryRun() {
+  return process.argv.includes("--dry-run");
+}
+
+function getLocalRepoUrl() {
+  const rootDir = execSync("git rev-parse --show-toplevel").toString().trim();
+
+  return `file://${rootDir}/.git`;
+}
+
+function getCurrentBranch() {
+  return execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
+}
+
+function getDryRunConfig() {
+  return {
+    repositoryUrl: getLocalRepoUrl(),
+    branches: [getCurrentBranch()],
+    preset: "conventionalcommits",
+    plugins: [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
     ],
-    [
-      "@semantic-release/github",
-      {
-        assets: ["*.tgz"],
-      },
-    ],
-  ],
-};
+  };
+}
+
+module.exports = isDryRun()
+  ? getDryRunConfig()
+  : {
+      branches: ["main"],
+      preset: "conventionalcommits",
+      plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+          "@semantic-release/npm",
+          {
+            npmPublish: false,
+            tarballDir: ".",
+          },
+        ],
+        [
+          "@semantic-release/github",
+          {
+            assets: ["*.tgz"],
+          },
+        ],
+      ],
+    };
