99f1359a4cee2a16dd16f003ea12606eae10f88e
Mike Dean <mike.dean@talis.com>
2024-10-01 15:59:01 +0100

    fix: update partitioned configs to share proxy config

--- lib/web-service/nginx-util.ts
+++ lib/web-service/nginx-util.ts
@@ -88,14 +88,21 @@ function getDefaultConfig(
     throw new Error("Application and nginx ports must be different");
   }
 
+  const defaultRouteLocation = createProxyRouteConfig(
+    "/",
+    "http://application",
+  );
+
+  const partitionedCookieLocations = getPartitionedCookiesConfig(
+    props.usePartionedCookiesLocations,
+  );
+
   return fs
     .readFileSync(resolvePath("nginx/default.conf"), "utf8")
     .replaceAll("{{applicationPort}}", applicationPort.toString())
     .replaceAll("{{nginxPort}}", nginxPort.toString())
-    .replaceAll(
-      "{{partitionedCookieConfig}}",
-      getPartitionedCookiesConfig(props.usePartionedCookiesLocations),
-    );
+    .replaceAll("{{defaultRouteLocation}}", defaultRouteLocation)
+    .replaceAll("{{partitionedCookieLocations}}", partitionedCookieLocations);
 }
 
 /**
@@ -108,24 +115,39 @@ function getSameSiteCookiesConfig(): string {
   return fs.readFileSync(resolvePath("nginx/samesite.conf"), "utf8");
 }
 
-/**
- * Return the contents of an Nginx configuration file that patches
- * `Set-Cookie` headers to use the `Partitioned` flag
- *
- * The output of this function is used with `createConfigMap` with `usePartionedCookiesLocations` provided.
- */
 function getPartitionedCookiesConfig(locations?: string[]): string {
   if (!locations) {
     return "";
   }
 
   return locations
-    .map(
-      (location) => `location ${location} {
-      proxy_cookie_path / "/; Partitioned";
-    }`,
+    .map((location) =>
+      createProxyRouteConfig(location, `http://application${location}`, [
+        `proxy_cookie_path / "/; Partitioned";`,
+      ]),
     )
     .join("\n\n");
+}
+
+function createProxyRouteConfig(
+  location: string,
+  proxyPath: string,
+  additionalSettings?: string[],
+): string {
+  const additional = additionalSettings ? additionalSettings.join("\n") : "";
+
+  return `location ${location} {
+    proxy_pass ${proxyPath};
+    proxy_http_version 1.1;
+
+    proxy_set_header Connection $connection_upgrade;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+    ${additional}
+  }`;
 }
 
 export const nginxUtil = {
